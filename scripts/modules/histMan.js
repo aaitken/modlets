88be4d5704ac40e6cae3db1baa0bcf9b295d49fa
Alex Aitken <aaitken@alexaitken.net>
2011-10-12 20:03:51 -0600

    histMan module added

---
+++ scripts/modules/histMan.js
@@ -0,0 +1,23 @@
+//AMA 2011
+//Normalize x-browser implementation of the history API
+
+define([],function(){
+	var histMan={
+
+		//init normalizes x-browser variations in firing of popstate on first page load
+		//logic lifted from pjax
+		//fn = function to fire on firing of popstate event
+		init:function(fn){
+			var popped=('state' in window.history),
+				initialURL = location.href;
+			window.onpopstate=function(e) {
+				var initialPop=!popped&&location.href==initialURL;
+				popped = true;
+				if(initialPop){return}
+				else{fn()}
+			};
+		}
+	};
+
+	return histMan;
+});
